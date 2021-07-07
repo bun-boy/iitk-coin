d3c4f0347f128d8bf81453621632c478bc2b4f7a
bun-boy <byash.iitk@gmail.com>
2021-07-07 16:46:51 +0530

    getCoinsHandler

---
+++ handlers/getCoinsHandler.go
@@ -0,0 +1,55 @@
+package handlers
+
+import (
+	"encoding/json"
+	"fmt"
+	"net/http"
+
+	"github.com/bun-boy/iitk-coin/utils"
+	_ "github.com/mattn/go-sqlite3"
+)
+
+func GetCoinsHandler(w http.ResponseWriter, r *http.Request) {
+	if r.URL.Path != "/getcoins" {
+		resp := &serverResponse{
+			Message: "404 Page not found",
+		}
+		JsonRes, _ := json.Marshal(resp)
+		w.Write(JsonRes)
+		return
+	}
+	c, err := r.Cookie("token")
+	if err != nil {
+		if err == http.ErrNoCookie {
+			http.Error(w, "", http.StatusUnauthorized)
+			return
+		}
+	}
+	tokenFromUser := c.Value
+	rollno, _, _ := utils.ExtractTokenMetadata(tokenFromUser)
+	w.Header().Set("Content-Type", "application/json")
+	resp := &serverResponse{
+		Message: "",
+	}
+	switch r.Method {
+	case "GET":
+		coins, err := utils.GetCoinsFromRollNo(rollno)
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusBadRequest)
+			fmt.Fprintf(w, " -User not found")
+			return
+		}
+		w.WriteHeader(http.StatusOK)
+		resp.Message = "Your coins are " + fmt.Sprintf("%f", coins)
+		JsonRes, _ := json.Marshal(resp)
+		w.Write(JsonRes)
+		return
+	default:
+		w.WriteHeader(http.StatusBadRequest)
+		resp.Message = "Sorry, only GET requests allowed!"
+		JsonRes, _ := json.Marshal(resp)
+		w.Write(JsonRes)
+		return
+	}
+
+}
